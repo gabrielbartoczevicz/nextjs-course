3cb46536087aa6375fd0c629832ba684163c514f
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-12-05 13:33:40 -0300

    feat: Client Side Fetching

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from 'react';
 import { Title } from '../styles/pages/Home';
 
-const Home = () => (
-  <div>
-    <Title>Henlo World</Title>
-    <strong>/</strong>
-  </div>
-);
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+const Home = () => {
+  const [recommendedProducts, setRecommendedProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3333/recommended').then(response => {
+      response.json().then(data => setRecommendedProducts(data))
+    });
+  }, []);
+
+  return (
+    <>
+      <Title>Henlo World</Title>
+
+      <section>
+        <Title>Products</Title>
+
+        <ul>
+          {recommendedProducts.map(product => (
+            <li key={product.id} >
+              {product.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
+  )
+};
 
 export default Home;
