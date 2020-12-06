e2bb9c09f81ba064eb4317486597adb6dcd6eeb7
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2020-12-06 17:26:24 -0300

    feat: custom document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { DocumentContext } from 'next/document'
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 class _Document extends Document {
@@ -26,6 +26,29 @@ class _Document extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head>
+          <meta charSet="utf-8" />
+
+          <link 
+            rel="preconnect" 
+            href="https://fonts.gstatic.com"
+          />
+          <link 
+            rel="stylesheet" 
+            href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" 
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
 
